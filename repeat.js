42087f28373d5199e0915effcaf0f9be522b2324
smb-it-coder <112647012+smb-it-coder@users.noreply.github.com>
2024-04-27 13:45:47 +0530

    Create repeat.js

---
+++ repeat.js
@@ -0,0 +1,28 @@
+//uncompress('3(ab)') // 'ababab'
+//uncompress('3(ab2(c))') // 'abccabccabcc'
+
+var rNum = 0;
+var rStr = '';
+var out = '';
+for(let char of str)  { 
+
+    if (typeof char === 'string' && (char == '(' || char == ')') ) {
+        continue;
+    } else if (!isNaN(char)) {
+        if(rStr != ''){
+            out += rStr.repeat(rNum);
+            rNum = parseInt(char);
+            rStr = '';
+        } else {
+            rNum = parseInt(char);
+        }
+        
+        console.log('Inside number', parseInt(char));
+    } else if (typeof char === 'string') {
+        rStr += char;
+        console.log('Inside string', char);
+    }
+  
+}
+
+out += rStr.repeat(rNum);
